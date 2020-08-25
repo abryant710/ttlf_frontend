ed778cf961781703af738f9f7caabbd14619f9ac
Alex Bryant <alexbryant710@gmail.com>
2020-08-25 22:19:38 +0100

    Adding basic site with construction message

---
+++ README.md
@@ -0,0 +1,5 @@
+# Trip the Light Fantastic Website 
+
+## Frontend
+
+Create React App has been used to build the frontend of this website
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './sass/main.sass';
+
+function App() {
+  return (
+    <div className="home">
+      <div className="home__image-container">
+        <h1 className="home__title">Trip the Light Fantastic</h1>
+        <img src="images/ttlf_0001.jpg" alt="TTFL" />
+        <h3 className="home__message">website under construction...</h3>
+      </div>
+    </div>
+  );
+}
+
+export default App;
