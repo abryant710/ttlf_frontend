7355e2d03e4c5fa8b2522d90fdf92aa23f2c64b7
Alex Bryant <alexbryant710@gmail.com>
2020-09-10 22:43:21 +0100

    Improved quality of holding page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './sass/main.sass';
+import { links } from './config/links';
 
 function App() {
   return (
     <div className="home">
       <h1 className="home__title">Trip the Light Fantastic</h1>
+      <h5 className="home__message">The website is currently under construction and we will have more content soon!</h5>
       <img className="home__image" src="images/ttlf_0001.jpg" alt="TTFL" />
-      <h3 className="home__message">website under construction...</h3>
+      <h3 className="home__link">
+        <a href={links.facebook} className="fb-link">Check out our Facebook page</a>
+      </h3>
     </div>
   );
 }
---
+++ src/config/links.ts
@@ -0,0 +1,3 @@
+export const links = {
+  facebook: "https://www.facebook.com/TTLFharddance",
+};
