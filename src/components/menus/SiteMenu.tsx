63f966f1dde10419fd509a126c9a6a810bae7322
Alex Bryant <alexbryant710@gmail.com>
2020-10-22 00:11:52 +0100

    Creating menu component

---
+++ src/components/menus/SiteMenu.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import {useTranslation} from "react-i18next";
+
+const getMenuOptions = function (t: Function): Array<string> {
+  return [
+    t('menu.djProfiles'),
+    t('menu.liveSchedule'),
+    t('menu.videos'),
+    t('menu.mailingList'),
+  ];
+};
+
+function SiteMenu() {
+  const {t} = useTranslation('common');
+  return (
+    <div className="siteMenu">
+      {getMenuOptions(t).map((page, idx) => (
+        <h3 key={idx}>{page}</h3>
+      ))}
+    </div>
+  );
+}
+
+export default SiteMenu;
