3e4a9bf48b3d484a0e14a31a8bde5a217a7c9d4b
Alex Bryant <alexbryant710@gmail.com>
2020-10-31 16:53:24 +0000

    Linting update

--- src/components/BackToTop.tsx
+++ src/components/BackToTop.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from 'react';
 import { FaArrowCircleUp } from 'react-icons/fa';
 
 function BackToTop() {
--- src/pages/Live.tsx
+++ src/pages/Live.tsx
@@ -29,6 +29,6 @@ function Live() {
       </div>
     </div>
   );
-};
+}
 
 export default Live;
