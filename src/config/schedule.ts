c7eb27dab572dc2efd802629d7443e8596c109a7
Alex Bryant <alexbryant710@gmail.com>
2020-11-03 08:38:42 +0000

    Adding schedule data and title text data

---
+++ src/config/schedule.ts
@@ -0,0 +1,17 @@
+export default [
+  {
+    dj: 'Jonni Bryant',
+    date: '2020-11-07',
+    time: '14:00',
+  },
+  {
+    dj: 'Chris James',
+    date: '2020-11-08',
+    time: '16:00',
+  },
+  {
+    dj: 'Josh Brown',
+    date: '2020-11-08',
+    time: '18:00',
+  },
+];
