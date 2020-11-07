b7727d9d5417c6614257f87ce13a3713ecdcb547
Alex Bryant <alexbryant710@gmail.com>
2020-11-07 19:04:37 +0000

    Fixing schedule dates

--- src/config/schedule.ts
+++ src/config/schedule.ts
@@ -21,22 +21,22 @@ export default [
   },
   {
     dj: 'Chris James',
-    date: '2020-18-07',
+    date: '2020-11-14',
     time: '17:30',
   },
   {
     dj: 'Jonni Bryant',
-    date: '2020-18-07',
+    date: '2020-11-14',
     time: '18:30',
   },
   {
     dj: 'Josh Brown',
-    date: '2020-18-07',
+    date: '2020-11-14',
     time: '19:30',
   },
   {
     dj: 'Melon',
-    date: '2020-18-07',
+    date: '2020-11-14',
     time: '20:30',
   },
 ];
