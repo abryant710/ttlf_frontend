8f5a8e4313db649e3973e669bd217f4f67f8e797
Alex Bryant <alexbryant710@gmail.com>
2020-11-08 17:18:20 +0000

    Updated schedule

--- src/config/schedule.ts
+++ src/config/schedule.ts
@@ -39,4 +39,29 @@ export default [
     date: '2020-11-14',
     time: '20:30',
   },
+  {
+    dj: 'Melon',
+    date: '2020-11-28',
+    time: '13:00',
+  },
+  {
+    dj: 'Chris James',
+    date: '2020-11-28',
+    time: '15:00',
+  },
+  {
+    dj: 'Josh Brown',
+    date: '2020-11-28',
+    time: '17:00',
+  },
+  {
+    dj: 'Jonni Bryant',
+    date: '2020-11-28',
+    time: '19:00',
+  },
+  {
+    dj: 'Fil-EP',
+    date: '2020-11-28',
+    time: '21:00',
+  },
 ];
