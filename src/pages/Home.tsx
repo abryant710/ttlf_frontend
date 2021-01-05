3ec424a475a813e585fd00b99137e6e262c4cd3d
Alex Bryant <alexbryant710@gmail.com>
2021-01-05 22:47:06 +0000

    Now loading the api on load of the app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,41 +1,79 @@
 import React, { useEffect, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useHistory } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import getSiteConfig from '../api/adminApi';
 
+import { Context as ContentContext } from '../context/ContentContext';
 import { Context as AppContext } from '../context/AppContext';
 
 import Loader from '../components/Loader';
 import CustomToast from '../components/CustomToast';
 
-// TODO: possibly add this to context as initial page load could be elsewhere
-let initalLoad = true;
+const showEventToast = (t: (trans: string) => string) => {
+  toast.info(
+    <CustomToast
+      headerText={t('home.events.message')}
+      link={{
+        route: '/schedule',
+        text: t('home.events.linkText'),
+      }}
+    />,
+  );
+};
+
+const showErrorToast = (t: (trans: string) => string) => {
+  toast.error(
+    <CustomToast
+      headerText={t('home.loadError.message')}
+      link={{
+        route: '/',
+        text: t('home.loadError.linkText'),
+      }}
+    />,
+  );
+};
 
 function Home() {
   const { t } = useTranslation('common');
+  const history = useHistory();
   const {
     state: { backButtonShown, showPlayer },
     hideBackButton,
     turnPlayerOff,
   } = useContext(AppContext);
+  const {
+    state: { liveNow, upcomingEvent },
+    loadFetchedContent,
+  } = useContext(ContentContext);
+
+  useEffect(() => {
+    if (liveNow) {
+      history.push('/live');
+      turnPlayerOff(true);
+    } else if (upcomingEvent) {
+      showEventToast(t);
+    } else if (!showPlayer) {
+      turnPlayerOff(false);
+    }
+  }, [liveNow, upcomingEvent]);
+
+  useEffect(() => {
+    getSiteConfig().then((res) => {
+      const { status } = res;
+      if (status === 'success') {
+        loadFetchedContent(res);
+      } else {
+        showErrorToast(t);
+      }
+    });
+  }, []);
 
   useEffect(() => {
     if (backButtonShown) {
       hideBackButton();
     }
-    if (!showPlayer && initalLoad) {
-      turnPlayerOff(false);
-      initalLoad = false;
-      toast.info(
-        <CustomToast
-          headerText={t('home.events.message')}
-          link={{
-            route: '/schedule',
-            text: t('home.events.linkText'),
-          }}
-        />,
-      );
-    }
-  });
+  }, [backButtonShown, hideBackButton]);
 
   return (
     <div className="home margin-bottom-footer">
