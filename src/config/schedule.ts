91d91e9f24e34e797599f0fe444258d255fe5554
Alex Bryant <alexbryant710@gmail.com>
2021-01-05 20:15:07 +0000

    Updating outdated schedule

--- src/config/schedule.ts
+++ src/config/schedule.ts
@@ -1,67 +1,7 @@
 export default [
   {
-    dj: 'Chris James',
-    date: '2020-11-07',
+    dj: 'Jonni Bryant',
+    date: '2021-01-16',
     time: '17:30',
-  },
-  {
-    dj: 'Jonni Bryant',
-    date: '2020-11-07',
-    time: '18:30',
-  },
-  {
-    dj: 'Josh Brown',
-    date: '2020-11-07',
-    time: '19:30',
-  },
-  {
-    dj: 'Melon',
-    date: '2020-11-07',
-    time: '20:30',
-  },
-  {
-    dj: 'Chris James',
-    date: '2020-11-14',
-    time: '17:30',
-  },
-  {
-    dj: 'Jonni Bryant',
-    date: '2020-11-14',
-    time: '18:30',
-  },
-  {
-    dj: 'Josh Brown',
-    date: '2020-11-14',
-    time: '19:30',
-  },
-  {
-    dj: 'Melon',
-    date: '2020-11-14',
-    time: '20:30',
-  },
-  {
-    dj: 'Melon',
-    date: '2020-11-28',
-    time: '13:00',
-  },
-  {
-    dj: 'Chris James',
-    date: '2020-11-28',
-    time: '15:00',
-  },
-  {
-    dj: 'Josh Brown',
-    date: '2020-11-28',
-    time: '17:00',
-  },
-  {
-    dj: 'Jonni Bryant',
-    date: '2020-11-28',
-    time: '19:00',
-  },
-  {
-    dj: 'Fil-EP',
-    date: '2020-11-28',
-    time: '21:00',
   },
 ];
