6a30c6ae9b72a7967caf80169812b628e13005c6
Alex Bryant <alexbryant710@gmail.com>
2021-01-05 23:58:06 +0000

    Tidied up live page and added DJ name

--- src/pages/Live.tsx
+++ src/pages/Live.tsx
@@ -54,6 +54,43 @@ function Live() {
     }, 2000);
   };
 
+  const getRefreshedFrame = (refreshed: boolean) => {
+    if (refreshed) {
+      return <Loader showSpinner withClasses={['loadingAnimation', 'margin-top-medium', 'margin-bottom-medium']} />;
+    }
+    return (
+      <div className="live__video--player-content">
+        <iframe
+          title="ttlf_live"
+          src={`https://player.twitch.tv/?channel=ttlf001&parent=${window.location.hostname}&autoplay=true`}
+          width={`${getWidthOrHeight('width')}px`}
+          height={`${getWidthOrHeight('height')}px`}
+          frameBorder="0"
+          allowFullScreen
+        />
+        <iframe
+          className="live__video--player-chat"
+          title="ttlf_live_chat"
+          frameBorder="0"
+          scrolling="no"
+          id="chat_embed"
+          src={`https://www.twitch.tv/embed/ttlf001/chat?parent=${window.location.hostname}`}
+          width={`${getWidthOrHeight('width')}px`}
+          height={`${getWidthOrHeight('height')}px`}
+        />
+        <p>
+          Powered by
+          <a target="_blank" href="https://www.twitch.tv/" rel="noopener noreferrer">
+            {' '}
+            Twitch.tv
+          </a>
+        </p>
+      </div>
+    );
+  };
+
+  const vidClsStr = 'live__video--player-size-selected';
+
   return (
     <div className="live margin-bottom-footer">
       <Title text={t('live.title')} />
@@ -71,60 +108,27 @@ function Live() {
           <div className="live__video--player-size">
             {width > mediumRes[0] && (
               <Button
-                className={`live__video--player-size-s ${selectedSize === 's' ? 'live__video--player-size-selected' : ''
-                  }`}
+                className={`live__video--player-size-s ${selectedSize === 's' ? vidClsStr : ''}`}
                 onClick={() => setSelectedSize('s')}
                 text={t('live.buttonSmall')}
               />
             )}
             {width > mediumRes[0] && (
               <Button
-                className={`live__video--player-size-m ${selectedSize === 'm' ? 'live__video--player-size-selected' : ''
-                  }`}
+                className={`live__video--player-size-m ${selectedSize === 'm' ? vidClsStr : ''}`}
                 onClick={() => setSelectedSize('m')}
                 text={t('live.buttonMedium')}
               />
             )}
             {width > largeRes[0] && (
               <Button
-                className={`live__video--player-size-l ${selectedSize === 'l' ? 'live__video--player-size-selected' : ''
-                  }`}
+                className={`live__video--player-size-l ${selectedSize === 'l' ? vidClsStr : ''}`}
                 onClick={() => setSelectedSize('l')}
                 text={t('live.buttonLarge')}
               />
             )}
           </div>
-          {!frameRefreshed ? (
-            <div className="live__video--player-content">
-              <iframe
-                title="ttlf_live"
-                src={`https://player.twitch.tv/?channel=ttlf001&parent=${window.location.hostname}&autoplay=true`}
-                width={`${getWidthOrHeight('width')}px`}
-                height={`${getWidthOrHeight('height')}px`}
-                frameBorder="0"
-                allowFullScreen
-              />
-              <iframe
-                className="live__video--player-chat"
-                title="ttlf_live_chat"
-                frameBorder="0"
-                scrolling="no"
-                id="chat_embed"
-                src={`https://www.twitch.tv/embed/ttlf001/chat?parent=${window.location.hostname}`}
-                width={`${getWidthOrHeight('width')}px`}
-                height={`${getWidthOrHeight('height')}px`}
-              />
-              <p>
-                Powered by
-                <a target="_blank" href="https://www.twitch.tv/" rel="noopener noreferrer">
-                  {' '}
-                  Twitch.tv
-                </a>
-              </p>
-            </div>
-          ) : (
-              <Loader showSpinner withClasses={['loadingAnimation', 'margin-top-medium', 'margin-bottom-medium']} />
-            )}
+          {getRefreshedFrame(frameRefreshed)}
         </div>
       </div>
     </div>
