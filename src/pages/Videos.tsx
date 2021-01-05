f9dc57373a322c940156b308ec6456b9fe2d1c1c
Alex Bryant <alexbryant710@gmail.com>
2021-01-05 22:55:18 +0000

    Videos now using content from database

--- src/pages/Videos.tsx
+++ src/pages/Videos.tsx
@@ -1,12 +1,18 @@
 import React, { useContext, useState } from 'react';
 import ReactPlayer from 'react-player';
 import { useTranslation } from 'react-i18next';
+import { shuffle } from 'lodash';
 
+import { Context as ContentContext } from '../context/ContentContext';
 import { Context as AppContext } from '../context/AppContext';
 
 import Title from '../components/Title';
 import Loader from '../components/Loader';
-import { urlPrefix, videos } from '../config/youTubeVideos';
+
+interface YouTubeVideo {
+  url: string;
+  title: string;
+}
 
 function Videos() {
   const { t } = useTranslation('common');
@@ -14,50 +20,59 @@ function Videos() {
     state: { showPlayer },
     turnPlayerOff,
   } = useContext(AppContext);
+  const {
+    state: { contentLoaded, youTubeVideoPrefix, youTubeVideos, youTubeVideosRandomised },
+  } = useContext(ContentContext);
+  const videos = youTubeVideosRandomised ? shuffle(youTubeVideos) : youTubeVideos;
+
   const [videoPlaying, changeVideoPlaying] = useState(-1);
   const loadStates: { [key: string]: boolean } = {};
-  videos.forEach((video, idx) => {
+
+  videos.forEach((_video: YouTubeVideo, idx: number) => {
     loadStates[`video_${idx}`] = false;
     return null;
   });
+
   const [videosLoaded, setLoaded] = useState(loadStates);
+
   return (
     <div className="videos margin-bottom-footer">
       <Title text={t('videos.title')} />
-      {videos.map((video, idx) => {
-        const { title, url } = video;
-        return (
-          <div className="videos__video" key={title}>
-            <h2 className="videos__video--title">{title}</h2>
-            {!videosLoaded[`video_${idx}`] && (
-              <Loader showSpinner withClasses={['loadingAnimation', 'margin-top-medium']} />
-            )}
-            <div className="videos__video--player">
-              <ReactPlayer
-                url={`${urlPrefix}${url}`}
-                width="100%"
-                height="100%"
-                config={{
-                  youtube: {
-                    playerVars: {
-                      showinfo: 1,
-                      controls: 1,
+      {contentLoaded &&
+        videos.map((video: YouTubeVideo, idx: number) => {
+          const { title, url } = video;
+          return (
+            <div className="videos__video" key={title}>
+              <h2 className="videos__video--title">{title}</h2>
+              {!videosLoaded[`video_${idx}`] && (
+                <Loader showSpinner withClasses={['loadingAnimation', 'margin-top-medium']} />
+              )}
+              <div className="videos__video--player">
+                <ReactPlayer
+                  url={`${youTubeVideoPrefix}${url}`}
+                  width="100%"
+                  height="100%"
+                  config={{
+                    youtube: {
+                      playerVars: {
+                        showinfo: 1,
+                        controls: 1,
+                      },
                     },
-                  },
-                }}
-                onReady={() => {
-                  setLoaded({ ...videosLoaded, [`video_${idx}`]: true });
-                }}
-                onPlay={() => {
-                  turnPlayerOff(true);
-                  changeVideoPlaying(idx);
-                }}
-                playing={!showPlayer && videoPlaying === idx}
-              />
+                  }}
+                  onReady={() => {
+                    setLoaded({ ...videosLoaded, [`video_${idx}`]: true });
+                  }}
+                  onPlay={() => {
+                    turnPlayerOff(true);
+                    changeVideoPlaying(idx);
+                  }}
+                  playing={!showPlayer && videoPlaying === idx}
+                />
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </div>
   );
 }
