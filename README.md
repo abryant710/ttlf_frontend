a57471e186eba1ed418eef6ecfb4ebd0d557325e
Alex Bryant <alexbryant710@gmail.com>
2021-06-21 09:45:28 +0100

    Tidying up scripts and adding more lazy loading for code splitting

--- README.md
+++ README.md
@@ -3,3 +3,23 @@
 ## Frontend
 
 Create React App has been used to build the frontend of this website
+
+[https://ttlf.net](https://ttlf.net)
+
+### Dev
+
+Run `npm run start` to run the development server
+
+### Linting code
+
+Use the linting scripts in `package.json` to lint the code as required
+
+### Deployment
+
+The app is deployed to AWS S3. Use the scripts in `package.json` to redeploy the app. 
+
+### Backend
+
+This app does have a backend built for configuration, but it is currently disabled to lower the cost of keeping the site active. At the moment static JSON data is being served for the configurable options.
+
+[https://admin.ttlf.net](https://admin.ttlf.net)
--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,22 @@ import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-d
 import { ToastContainer, Zoom, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+// NOTE: Only used if backend active
 // import getSiteConfig from './api/adminApi';
 import staticJSON from './api/static.json';
 import { Context as ContentContext } from './context/ContentContext';
 
-import Home from './pages/Home';
-import DJProfiles from './pages/DJProfiles';
-import Videos from './pages/Videos';
-import LiveSchedule from './pages/LiveSchedule';
-import Live from './pages/Live';
+import Loader from './components/Loader';
 import Header from './components/nav/Header';
 import Footer from './components/nav/Footer';
 import SiteMenu from './components/menus/SiteMenu';
 import './sass/main.sass';
 
+const Home = lazy(() => import('./pages/Home'));
+const DJProfiles = lazy(() => import('./pages/DJProfiles'));
+const Videos = lazy(() => import('./pages/Videos'));
+const LiveSchedule = lazy(() => import('./pages/LiveSchedule'));
+const Live = lazy(() => import('./pages/Live'));
 const Toast = lazy(() => import('./components/CustomToast'));
 const ToTop = lazy(() => import('./components/BackToTop'));
 
@@ -41,10 +43,12 @@ function App() {
 
   useEffect(() => {
     const loadApp = async () => {
+      // NOTE: Only used if backend active
       // const res = await getSiteConfig();
       // const { status } = res;
       const { status } = staticJSON;
       if (status === 'success') {
+        // NOTE: Only used if backend active
         // loadFetchedContent(res);
         loadFetchedContent(staticJSON);
       } else {
@@ -59,11 +63,17 @@ function App() {
       <Header />
       <SiteMenu />
       <Switch>
-        <Route path="/bios" exact component={DJProfiles} />
-        <Route path="/videos" exact component={Videos} />
-        <Route path="/schedule" exact component={LiveSchedule} />
-        <Route path="/live" exact component={Live} />
-        <Route path="/" exact component={Home} />
+        <Suspense
+          fallback={
+            <Loader showSpinner withClasses={['loadingAnimation', 'margin-top-medium', 'margin-bottom-medium']} />
+          }
+        >
+          <Route path="/bios" exact component={DJProfiles} />
+          <Route path="/videos" exact component={Videos} />
+          <Route path="/schedule" exact component={LiveSchedule} />
+          <Route path="/live" exact component={Live} />
+          <Route path="/" exact component={Home} />
+        </Suspense>
         <Redirect to="/" />
       </Switch>
       <Footer />
